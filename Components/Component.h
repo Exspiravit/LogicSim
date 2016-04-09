5ba5cb059573e66e4e3b2f8ccd9f46bcb187328f
TheAbzo <soon-ab@hotmail.com>
2016-04-09 22:47:19 +0200

    Update Component.h

--- Components/Component.h
+++ Components/Component.h
@@ -8,16 +8,26 @@ class Output;
 
 class Component
 {
-protected:
+private:
 	GraphicsInfo m_GfxInfo;
 	std::string m_Label;
+	bool m_Selected;
 
 public:
-	Component(const GraphicsInfo &r_GfxInfo, std::string r_Label);
+	Component(const GraphicsInfo &r_GfxInfo, std::string r_Label,bool r_Selected=true);
+	virtual ~Component();
+
+	const GraphicsInfo& GetGraphicsInfo() const;
+	void SetGraphicsInfo(const GraphicsInfo  &r_GfxInfo) ;
+	std::string GetLabel();
+	void SetLabel(std::string r_Label);
+	void SetSelcted(bool r_Selected);
+
 	virtual void Operate()=0;
 	virtual void Draw(Output* pOut)=0;
 	virtual int GetOutputPinStatus()=0;
 	virtual int GetInputPinStatus(int n)=0;
 	virtual void SetInputPinStatus(int n, STATUS s)=0;
-	virtual ~Component();
+	
 };
+
