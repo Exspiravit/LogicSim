82cfd20ab7437941c634d7d561be8586ed487f93
Exspiravit <Exspiravit@users.noreply.github.com>
2016-04-09 14:54:11 +0200

    Modifying GraphicsInfo

--- Components/Component.cpp
+++ Components/Component.cpp
@@ -1,11 +1,7 @@
 #include "Component.h"
 
-Component::Component(const GraphicsInfo &r_GfxInfo, std::string r_Label)
+Component::Component(const GraphicsInfo &r_GfxInfo, std::string r_Label): m_GfxInfo(r_GfxInfo)
 {
-    m_GfxInfo.x = r_GfxInfo.x;
-    m_GfxInfo.y = r_GfxInfo.y;
-    m_GfxInfo.Height = r_GfxInfo.Height;
-    m_GfxInfo.Width = r_GfxInfo.Width;
     m_Label = r_Label;
 }
 
