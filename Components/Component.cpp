383aafb0e693cd8810c3a943930dd51f08c3c9ce
TheAbzo <soon-ab@hotmail.com>
2016-04-09 22:45:17 +0200

    Update Component.cpp

--- Components/Component.cpp
+++ Components/Component.cpp
@@ -1,8 +1,24 @@
 #include "Component.h"
 
-Component::Component(const GraphicsInfo &r_GfxInfo, std::string r_Label): m_GfxInfo(r_GfxInfo)
+Component::Component(const GraphicsInfo &r_GfxInfo, std::string r_Label, bool isSelcted ) : m_GfxInfo(r_GfxInfo), m_Selected(isSelcted)
 {
-    m_Label = r_Label;
+	SetLabel(r_Label);
 }
 
 Component::~Component(){}
+const GraphicsInfo& Component::GetGraphicsInfo() const
+{
+	return m_GfxInfo;
+}
+void Component::SetGraphicsInfo(const GraphicsInfo  &r_GfxInfo)
+{
+	m_GfxInfo = r_GfxInfo;
+}
+std::string Component::GetLabel()
+{
+	return m_Label;
+}
+void Component::SetLabel(std::string r_Label)
+{
+	m_Label = r_Label;
+}
